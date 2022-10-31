a70fe670ebec38c1900c353dbbdb84639f162912
Mr SK <101137335+mrsk216@users.noreply.github.com>
2022-11-01 00:18:20 +0600

    mouse move

--- asset/js/hero_cursor.js
+++ asset/js/hero_cursor.js
@@ -1,25 +1,9 @@
-document.onmousemove = animatedmousemoves;
+const bg = document.querySelector('#hero');
+const walk = 50;
 
-const colors = ['#191A1C', '#00059b', '#ff6347'];
-
-function animatedmousemoves(event) {
-    let mousemove = document.createElement('div');
-    mousemove.setAttribute('class', 'mousemove');
-    document.body.appendChild(mousemove);
-
-    mousemove.style.left = event.clientX + 'px';
-    mousemove.style.top = event.clientY + 'px';
-
-    let color = colors[Math.floor(Math.random() * colors.length)];
-    mousemove.style.borderColor = color;
-
-    mousemove.style.transition = 'all 2s linear 0s';
-
-    mousemove.style.left = mousemove.offsetLeft - 20 + 'px';
-    mousemove.style.top = mousemove.offsetTop - 20 + 'px';
-
-    mousemove.style.width = '100px';
-    mousemove.style.height = '50px';
-    mousemove.style.borderWidth = '8px';
-    mousemove.style.opacity = 0;
-}
+const move = (e) => {
+    let mouseX = -Math.round((e.clientX / window.innerWidth) * walk - (walk / 2));
+    let mouseY = -Math.round((e.clientY / window.innerHeight) * walk - (walk / 2));
+    bg.style.transform = `scale(1.1) translate(${mouseX}px, ${mouseY}px)`;
+};
+window.addEventListener('mousemove', move);
